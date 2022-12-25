da51019ffc95f2d07cfdb4f34d89181f40d38396
minhkhiemm <vominhkhiem212@gmail.com>
2022-12-25 20:26:26 +0700

    add react and react dom

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     resolve: {
         extensions: [".js", ".ts", ".tsx"],
     },
-    entry: ["@babel/polyfill", "./main.ts"],
+    entry: ["@babel/polyfill", "./index.tsx"],
     output: {
         path: path.join(basePath, "dist"),
         filename: "bundle.js",

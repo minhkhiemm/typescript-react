1e6a330f55ec90c1ff949c8605a7076f1d5d4026
minhkhiemm <vominhkhiem212@gmail.com>
2022-12-25 20:54:39 +0700

    username now can receive input from user

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-
-import { HelloComponent } from './hello';
+import { App } from './app';
 
 ReactDOM.render(
-    <HelloComponent userName="Khiem" />,
+    <App />,
     document.getElementById('root')
 );
